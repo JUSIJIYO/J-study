3aa7d966d19ce8c8e9043f01b71dd5997e9f6fb4
biyuns <cart0089@naver.com>
2026-03-31 18:08:56 +0900

    03_31_practice code

--- Jihoon/03_30_practice/src/App.jsx
+++ Jihoon/03_30_practice/src/App.jsx
@@ -1,6 +1,7 @@
 import Counter from "./components/Counter";
 import Favorite from "./components/Favorite";
 import Hobby from "./components/Hobby";
+import Hooks from "./components/hooks";
 import ToggleSwitch from "./components/ToggleSwitch";
 import UserInfo from "./components/UserInfo";
 
@@ -8,30 +9,33 @@ function App() {
   return (
     <>
 
-      {/* 1) 카운터 컴포넌트 제작 */}
+
+      {/* 1) 카운터 컴포넌트 제작
       <h2>카운터</h2>
       <Counter />
       <hr />
 
-      {/* 2) 사용자 정보 컴포넌트 제작 */}
+      {/* 2) 사용자 정보 컴포넌트 제작 
       <h2>사용자 정보</h2>
       <UserInfo />
       <hr />
 
-      {/* 3) 토글 스위치 컴포넌트 제작 */}
+      {/* 3) 토글 스위치 컴포넌트 제작 
       <h2>토글 스위치</h2>
       <ToggleSwitch />
       <hr />
 
-      {/* 4) 취향 선택 컴포넌트 제작 */}
+      {/* 4) 취향 선택 컴포넌트 제작 
       <h2>취향 선택</h2>
       <Favorite />
       <hr />
 
-      {/* 5) 취미 선택 컴포넌트 제작 */}
+      {/* 5) 취미 선택 컴포넌트 제작 
       <h2>취미 선택</h2>
       <Hobby />
-      <hr />
+      <hr /> */}
+
+      <Hooks/>
     </>
   );
 }

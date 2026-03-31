6716552208d09cb3ee1a10314bf3c1a791a5e74b
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-03-31 18:09:07 +0900

    0331 과제

---
+++ Siyun/0330/99_usestate-practice/src/components/Hooks.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+function Hooks() {
+  const [targetText, setTargetText] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const input = prompt('문자열을 입력해주세요');
+    setTargetText(input);
+    setTime(Date.now());
+  }, []);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInputText(value);
+
+    if (value == targetText && targetText !== '') {
+      const endTime = Date.now();
+      const result = endTime - time;
+      alert(result + '(ms)');
+      // setInputText('');
+      // setTargetText('');
+      return;
+    }
+  };
+  return (
+    <>
+      <h2>{targetText}</h2>
+      <input type="text" value={inputText} onChange={handleChange} />
+    </>
+  );
+}
+
+export default Hooks;

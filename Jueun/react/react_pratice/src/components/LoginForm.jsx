75727330e83049af30eba3c39d8da171563cd1c7
jia40 <lsc7439@g.eulji.ac.kr>
2026-04-03 20:31:27 +0900

    0403

--- Jueun/react/react_pratice/src/components/LoginForm.jsx
+++ Jueun/react/react_pratice/src/components/LoginForm.jsx
@@ -5,6 +5,27 @@ function LoginForm() {
     const [name, setName]=useState('');
     const [password, setPassword]=useState('');
 
+    const [check,setCheck]=useState(false);
+
+    const handleCheck = () =>{
+      setCheck(!check);
+    }
+    // 내가 입력한 이름
+    const inputName = (e)=>{
+      setName(e.target.value)
+
+    }
+    
+    const inputPw = (e)=>{
+      setPassword(e.target.value)
+    }
+
+    const btnClick = () => {
+      alert(`로그인 시도!
+        사용자: ${name}
+        기억하기: ${check ? "예": "아니요"} `
+      )
+    }
   return (
     <>
       <h2>로그인 폼</h2>
@@ -14,6 +35,7 @@ function LoginForm() {
       type="text"
       value={name}
       placeholder='사용자명'
+      onChange={inputName}
       />
       <br />
 
@@ -22,14 +44,18 @@ function LoginForm() {
       type="password"
       value={password}
       placeholder='비밀번호'
+      onChange={inputPw}
       />      
       <br />
 
       <input
       type="checkbox"
-      /> 로그인 상태 유지
-
+      checked={check}
+      onChange={handleCheck}
       
+      /> 로그인 상태 유지
+    <br/>
+      <input type="button" onClick={btnClick} value="로그인"/>
     </>
   )
 }
--- Jueun/react/react_pratice/src/components/ToDo.jsx
+++ Jueun/react/react_pratice/src/components/ToDo.jsx
@@ -4,7 +4,6 @@ import { NameContext } from '../App'
 function ToDo() {
     
     const [text, setText]=useState(''); //입력중인 아이
-    
     const {names,setNames} = useContext(NameContext);
 
     const handleAddChange = (e) => {
@@ -12,12 +11,27 @@ function ToDo() {
     }
     
     const addClick= () => {
-        setNames([...names, text])
+        setNames([...names, 
+          { 
+            text: text, 
+            check: false
+           }])
         setText('')
     }
 
-    const deleteClick = () => {
+    const deleteClick = (index) => {
+      const DeleteNames=names.filter((_,i) => i !==index); //_: 현재 검사중인 글자 내용-> 지금은 인덱스 번호만 필요해서 무시하겠다, i: 인덱스(순서)
+      //i !==index 내가 클릭한 번호랑 다른 인덱스만
+      setNames(DeleteNames) // 남아있어라(사라질 애들만 사라지고 남은 친구들 화면에 그려주는거)
+    }
 
+    const handleCheck = (addIndex) => { 
+        const updatedNames = names.map((item, index) => { 
+            return index === addIndex 
+            ? {...item, check: !item.check}  // 선택한 것만 check 반전
+            : item 
+        })
+        setNames(updatedNames)
     }
 
   return (
@@ -29,16 +43,24 @@ function ToDo() {
      
       <br />
 
-        {names.map((name,index) => (
-           <div 
-           key={index}>
-            <input type="checkbox"/> {name}
-            <button onClick={()=>deleteClick(index)}>삭제</button>
-           </div>
-    
-        ))}
+      {names.map((item, index) => (
+        <div key={index}>
+          <input
+            type="checkbox"
+            checked={item.check}
+            onChange={() => handleCheck(index)} // 내가 선택한 요소만 체크되게
+          />
 
-  
+          <span style={{
+            textDecoration: item.check ? 'line-through' : 'none' // 체크하면 취소선
+          }}>
+            {item.text}
+          </span>
+
+          <button onClick={() => deleteClick(index)}>삭제</button>
+        </div>
+      ))}
+
        <br />
 
     </>
